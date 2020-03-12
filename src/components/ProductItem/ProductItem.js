c202faa4ba31ff34c16b60fab064cd7dd3c4f50e
Maksym Arabadzhyi <arabadzhyi.tomnium@gmail.com>
2020-03-12 11:53:57 +0200

    product item component added

---
+++ src/components/ProductItem/ProductItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ProductItem.scss';
+
+export default function ProductItem() {
+  const {product} = this.props;
+  return (
+    <div className="ProductItem">
+      <div className="card">
+        <h5 className="card__title">{product.title}</h5>
+        <label className="description">Description:</label>
+        <p>{product.description}</p>
+        <label className="description">Price:</label>
+        <p>{product.price}₴</p>
+        <div className="card__button-wrapper">
+          <button className="card__button" onClick={() => {addToCart(product.id)}}>
+            <FontAwesomeIcon icon='cart-plus'/>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+} 
