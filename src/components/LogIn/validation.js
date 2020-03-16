48867833b0560e0d0a0b59a62aad69fa4ccf2121
Maksym Arabadzhyi <arabadzhyi.tomnium@gmail.com>
2020-03-16 10:48:07 +0200

    login validation updated

--- src/components/LogIn/validation.js
+++ src/components/LogIn/validation.js
@@ -1,3 +1,3 @@
 export const required = value => value ? undefined : 'Required';
 export const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined;
-export const password = value => value < 8 ? 'Password must be at least 8 characters long' : undefined;
+export const password = value => value.length < 8 ? 'Password must be at least 8 characters long' : undefined;
