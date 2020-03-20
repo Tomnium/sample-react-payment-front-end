ffcb6484b60f91dd99a993629ce42e1b90bb9cc6
Maksym Arabadzhyi <arabadzhyi.tomnium@gmail.com>
2020-03-20 13:03:55 +0200

    dynamic visualization products in cart

--- src/containers/CartContainer/CartContainer.js
+++ src/containers/CartContainer/CartContainer.js
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CartList from '../../components/CartList/CartList';
 import { getCartProducts } from '../../redux/actions';
 
 const CartContainer = () => {
-  const cartProducts = useSelector(state => state.cart.idList);
+  const productsId = useSelector(state => state.cart.idList);
+  const cartProducts = useSelector(state => state.cart.cartProducts);
+  const totalPrice = useSelector(state => state.cart.totalPrice);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getCartProducts(cartProducts))
+    dispatch(getCartProducts(productsId))
   }, []);
   return (
-    <CartList />
+    <Fragment>
+      {
+        cartProducts && cartProducts.length !== 0 ?
+        <CartList products={cartProducts} total={totalPrice}/> :
+        <h2>Your shopping cart is empty</h2>
+      }
+    </Fragment>
   )
 }
 
