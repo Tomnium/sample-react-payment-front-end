d9a6e5f5215ade50114ae126ebbae26170a67489
Maksym Arabadzhyi <arabadzhyi.tomnium@gmail.com>
2020-03-23 15:06:15 +0200

    logging in functionality added

---
+++ src/containers/LogInContainer/LogInContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LogIn from '../../components/LogIn/LogIn';
+import { useDispatch } from 'react-redux';
+import { logIn } from '../../redux/actions';
+
+const LogInContainer = props => {
+  const dispatch = useDispatch();
+  const submit = form => {
+    dispatch(logIn(form.email, form.password));
+    props.history.push('/');
+  }
+  return (
+    <LogIn onSubmit={submit} />
+  )
+}
+
+export default LogInContainer;
