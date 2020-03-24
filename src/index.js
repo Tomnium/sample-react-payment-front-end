0020b16953c295dc10681d1ba3fccfdbce1a9d6b
Maksym Arabadzhyi <arabadzhyi.tomnium@gmail.com>
2020-03-24 17:19:52 +0200

    redux store creating in a separate file

--- src/index.js
+++ src/index.js
@@ -3,32 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/App/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { reducer as formReducer } from 'redux-form';
-import { productReducer } from './redux/reducers/productReducer';
-import logger from 'redux-logger';
-import { cartReducer } from './redux/reducers/cartReducer';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
-import { authReducer } from './redux/reducers/authReducer';
-
-const reducers = {
-  form: formReducer,
-  products: productReducer,
-  cart: cartReducer,
-  auth: authReducer
-};
-
-const rootReducer = combineReducers(reducers);
-
-const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, promise, logger)));
+import store from './redux';
 
 ReactDOM.render(
   <Provider store={store}>
